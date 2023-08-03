978d0e9b86945b36e603b877a108c3f740cd0134
aijc <13271104+aijc_coder@user.noreply.gitee.com>
2023-08-03 14:26:54 +0800

    5

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,23 +1,26 @@
 <template>
     <div class="page">
-        <el-form class="login-form" ref="form" label-width="100">
-            <el-form-item label="Username">
-                <el-input v-model="model.username"/>
+        <el-form class="login-form" ref="form" label-width="100" :model="model" :rules="rules">
+            <el-form-item label="Username" prop="username" >
+                <el-input v-model="model.username" />
             </el-form-item>
-            <el-form-item label="Password">
-                <el-input v-model="model.password"/>
+            <el-form-item label="Password" prop="password">
+                <el-input v-model="model.password" />
             </el-form-item>
-            <el-form-item label="VerifyCode" class="captcha">
+            <el-form-item label="VerifyCode" class="captcha" prop="verifyCode">
                 <el-input class="captcha__input" v-model="model.verifyCode" />
+                <div class="captcha__cap" @click="refresh()" v-html="svg"></div>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="handleLogin">Login</el-button>
             </el-form-item>
         </el-form>
     </div>
+
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
     name: "Login",
@@ -33,6 +36,12 @@ export default {
                 // 验证码
                 verifyCode: "",
             },
+            svg: "",
+            rules: {
+                username: [{ required: true, message: "请输入" }],
+                password: [{min:6, required: true, message: "请输入6位以上" }],
+                verifyCode: [{ required: true, message: "请输入" }],
+            }
 
         }
     },
@@ -40,7 +49,30 @@ export default {
     methods: {
         handleLogin() {
 
-        }
+        },
+        refresh() {
+        axios
+          .get("/api/admin/base/open/captcha")
+          .then((response) => {
+            // console.log("response", response);
+            const { data } = response;
+            // 判断业务状态码
+            if (data.code !== 1000) {
+              return alert(data.message);
+            }
+            // 将 data.data.data => this.svg
+            this.svg = data.data.data;
+
+            // console.log("this.svg", this.svg);
+          })
+          .catch((error) => {
+            console.log("error", error);
+          });
+    },
+    },
+
+    created(){
+        this.refresh()
     }
 
 
@@ -73,5 +105,11 @@ export default {
     &__input {
         width: 70px;
     }
+    &__cap{
+        width: 140px;
+        height: 40px;
+        background: #ccc;
+    }
 }
+
 </style>
